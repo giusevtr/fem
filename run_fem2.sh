3750a59395a9f29c1fc68a267ec5e03f51df37f8
Giuseppe Vietri <vietr002@umn.edu>
2021-02-25 16:10:55 -0600

    update

--- run_fem2.sh
+++ run_fem2.sh
@@ -7,7 +7,7 @@ declare -a noise="0.1 0.12 0.14 0.16 0.18";
 noise=
 eps0=
 #    python generate.py loans 24 5 0.1 0.15 0.20 0.25 0.5 1  newadafem --samples=100 --noise="$n" --eps0="$eps0"
-python fem.py adult 64 3 0.1 0.15 0.2 0.25 0.5 1 --samples=50 --noise_multiple="$n" --epsilon_split="$eps0" # 0.008 0.16 50
-python fem.py adult 64 5 0.1 0.15 0.2 0.25 0.5 1 --samples=50 --noise_multiple="$n" --epsilon_split="$eps0" # 0.01 0.16 50
-python fem.py loans 64 3 0.1 0.15 0.2 0.25 0.5 1 --samples=50 --noise_multiple="$n" --epsilon_split="$eps0" # 0.006 0.1 50
-python fem.py loans 64 5 0.1 0.15 0.2 0.25 0.5 1 --samples=50 --noise_multiple="$n" --epsilon_split="$eps0" # 0.007 0.14 50
+python fem.py adult 64 3 0.1 0.15 0.2 0.25 0.5 1 --samples=50 --noise_multiple=0.16 --epsilon_split=0.008 # 0.008 0.16 50
+python fem.py adult 64 5 0.1 0.15 0.2 0.25 0.5 1 --samples=50 --noise_multiple=0.16 --epsilon_split=0.010 # 0.01 0.16 50
+python fem.py loans 64 3 0.1 0.15 0.2 0.25 0.5 1 --samples=50 --noise_multiple=0.10 --epsilon_split=0.006 # 0.006 0.1 50
+python fem.py loans 64 5 0.1 0.15 0.2 0.25 0.5 1 --samples=50 --noise_multiple=0.14 --epsilon_split=0.007 # 0.007 0.14 50
